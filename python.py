8bfa48307b3ed5d75d9d79890b01b970f0785b12
Ankit Agrawal <36307591+ank7275@users.noreply.github.com>
2022-05-09 21:46:30 +0530

    Update python.py

--- python.py
+++ python.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # which tells the application which URL should call 
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+# URL is bound with hello_world() function.
 def hello_world():
     return 'Hello World'
   
