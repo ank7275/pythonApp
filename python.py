089f651a984995ab116182fc1c390ae419bc7135
Ankit Agrawal <36307591+ank7275@users.noreply.github.com>
2022-05-10 20:46:14 +0530

    Update python.py

--- python.py
+++ python.py
@@ -20,7 +20,7 @@ def shutdown_server():
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
     
-@app.get('/shutdown')
+@app.route('/shutdown')
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
